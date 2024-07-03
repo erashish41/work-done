c357566acfa20b5a635ac6673e8f725d0a4ea44d
Ashish Bhardwaj <erashish41@gmail.com>
2024-07-03 23:59:44 +0530

    string update

--- practice4/index.js
+++ practice4/index.js
@@ -1,7 +1,7 @@
 //  check the conditions of all Data types
 
 // 1. string
-let string = "Ashish";
+let string = "Ashish Bhardwaj";
 
 // a. typeof
 let typeof_string = (typeof string === "string")
@@ -13,27 +13,31 @@ if(typeof_string){
 let str_lenght =  (string.length == 6)
 console.log("length of string is eight", string, string.length, ": ", str_lenght);
 
-//c. convert to toUpperCase
-let str1 = "ashish"
-let strrrr = (string.toUpperCase === str1)
-if(strrrr){
-    console.log(strrrr, "dhfdsfhdsjfh");
-}
-console.log("////////////");
-
-
-test_string_one = "RajaT"  
-test_string_two = "rajat"
-
-console.log(test_string_one.toLowerCase() == test_string_two)
-console.log("ggggggg");
-console.log(string.startsWith("Asf"))
-console.log(test_string_two.endsWith("at"))
+// c. convert to toUpperCase
+// let string = "Ashish";
+let test_str1 = "ashish"
+console.log(string.toLowerCase() === test_str1,"this for upper case");
 
 let str2 = string.toLowerCase()
 console.log(str2);
 
-let str3 = string.slice(2,4)
+// d. find the start and end of string
+let string_starts = string.startsWith("As") 
+console.log(string_starts,"this is for start of string");
+
+let string_ends = string.endsWith("sh")
+console.log(string_ends,"this is for end of string");
+
+// e. split a string and convert it into an array 
+// let string = "Ashish";
+let string_to_array = function (e){
+    return e.trim().split(" ")
+} 
+let converted_string = string_to_array(string)
+console.log(converted_string," : string to array");
+
+// f. to extract a specified number of characters from a string
+let str3 = string.slice(0,4)
 console.log(str3);
 
 let str4 = string.slice(2)
@@ -68,3 +72,13 @@ let is_string = function(input){
 console.log(is_string('ashi'));
 console.log(is_string([1,2,3,4]));
 console.log("dsssssss");
+
+
+
+let truncate_string = function(string){
+    if(string.constructor === string){
+        return string.slice(0)
+    }
+}
+let fall = truncate_string(string)
+console.log(fall);
